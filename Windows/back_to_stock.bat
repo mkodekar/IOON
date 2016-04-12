d40689a516eef9f5ecd558263c6aadceb11ad5d0
William Todt <deltax.fluxion@gmail.com>
2016-04-12 21:31:31 +0300

    Create back_to_stock.bat

---
+++ Windows/back_to_stock.bat
@@ -0,0 +1,51 @@
+fastboot erase cache
+fastboot erase system 
+fastboot erase recovery 
+fastboot erase userdata
+fastboot erase userdata_64
+fastboot erase modem 
+fastboot erase modemst1 
+fastboot erase modemst2
+fastboot erase recovery
+fastboot flash aboot aboot.img
+fastboot flash aboot abootbak.img
+fastboot flash apdp apdp.img
+fastboot flash bluetooth bluetooth.img 
+fastboot flash boot boot.img
+fastboot flash cache cache.img
+fastboot flash config config.img 
+fastboot flash ddr ddr.img 
+fastboot flash devinfo devinfo.img
+fastboot flash dpo dpo.img
+fastboot flash driver driver.img
+fastboot flash fsc fsc.img
+fastboot flash fsg fsg.img
+fastboot flash hyp hyp.img
+fastboot flash hyp hypbak.img 
+fastboot flash keystore keystore.img
+fastboot flash limits limits.img
+fastboot flash LOGO logo.img
+fastboot flash misc misc.img
+fastboot flash modem modem.img
+fastboot flash msadp msadp.img
+fastboot flash oem_dycnvbk oem_dycnvbk.img
+fastboot flash oem_stanvbk oem_stanvbk.img
+fastboot flash param param.img
+fastboot flash persist persist.img
+fastboot flash pmic pmic.img
+fastboot flash pmic pmicbak.img
+fastboot flash reserve1 reserve1.img
+fastboot flash reserve2 reserve2.img
+fastboot flash rpm rpmbak.img
+fastboot flash recovery maxrecovery.img
+fastboot flash sbl1 sbl1.img
+fastboot flash sbl1 sbl1bak.img
+fastboot flash sdi sdi.img
+fastboot flash sec sec.img
+fastboot flash ssd ssd.img
+fastboot flash tz tz.img
+fastboot flash tz tzbak.img
+fastboot flash userdata userdata.img
+fastboot flash userdata_64 userdata_64G.img
+fastboot reboot 
+
